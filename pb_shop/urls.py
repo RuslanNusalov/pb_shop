153d9fa7ac7c52e83f9abb6699a5c7c7176c6a10
Ruslan Nusalov <ruslan.nusalov@gmail.com>
2026-07-29 00:02:08 +0300

    add path

--- pb_shop/urls.py
+++ pb_shop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# ✅ Явная отдача медиа-фافайлов в продакшене
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
